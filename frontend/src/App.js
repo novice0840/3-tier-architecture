1d4a5ba9b9ee05fa56dedff9fa112da5c761d6a6
lotus9630 <lotus9630@gmail.com>
2022-07-08 01:29:55 +0900

    frontend 도커파일로 이미지로 빌드에서 컨테이너화 해서 브라우저로 접속까지 성공

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Frontend 연결됨</h1>
+      <h1>Backend 연결됨</h1>
+      <h1>Database 연결됨</h1>
+    </div>
+  );
+}
+
+export default App;
